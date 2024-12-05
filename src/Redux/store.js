6e5166114d4fa8ab25f3523672bcf62c504ab8c3
Pavel Karliuk <pavelkarliuk1@gmail.com>
2024-12-05 18:23:22 +0300

    Dockerfile complete

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,10 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import { AuthReducer } from "./Slies/authSlice";
-// import { WorkerReducer } from "./Slies/workerSlice";
-// import { DirectorReducer } from "./Slies/directorSlice";
-// import { AccountantReducer } from "./Slies/accountantSlice";
-// import { ManagerReducer } from "./Slies/managerSlice";
 
 const store = new configureStore({
     reducer: {
